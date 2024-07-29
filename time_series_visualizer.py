3c4652c5196b89d97dd11e0b70b244975fde9bbe
Locuraal33 <locuraal32@gmail.com>
2024-07-29 17:46:10 -0500

    grafico

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -16,3 +16,22 @@ filtered_df = df[(df['value'] >= lower_bound) & (df['value'] <= upper_bound)]
 
 print("\nDatos después de la limpieza:")
 print(filtered_df.head())
+
+# 3.Crear la funcion para el grafico de lineas
+def draw_line_plot(df):
+    # Crear la figura y los ejes
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Dibujar el gráfico de líneas
+    ax.plot(df.index, df['value'], color='green', linewidth=1)
+
+    # Establecer el título y las etiquetas
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+
+    # Mostrar el gráfico
+    plt.show()
+
+# Suponiendo que filtered_df es el DataFrame filtrado después de limpiar los datos
+draw_line_plot(filtered_df)
